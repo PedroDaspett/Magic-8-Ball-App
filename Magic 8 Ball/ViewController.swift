22365f6f6f7dc1ff72f655ac1f4b2e4492993eea
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2021-09-11 14:07:40 -0300

    Adding app's images

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
 
 }
 
